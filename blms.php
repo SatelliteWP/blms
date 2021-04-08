8601753326b17983ea33ec2946ab00d7463ed0f5
moustaphasaba <moustaphasaba@gmail.com>
2021-04-08 15:16:04 +0000

    Rajout de la page de settings

--- blms.php
+++ blms.php
@@ -7,8 +7,67 @@
  * Author URI: https://blacklivesmatter.support/
 */
 
-add_action( 'wp_enqueue_scripts', 'blms_enqueue_script' );
+function blms_init() {
+  $labels = array(
+      'name'      => __( 'Parameters', 'blms' ),
+      'menu_name' => __( 'Black Lives Matter', 'blms' )
+  );
+  $args = array(
+      'labels'        => $labels,
+      'public'        => true,
+      'menu_position' => 80,
+      'capabilities'  => array(
+        'create_posts' => 'do_not_allow'
+      )
+  );
+  register_post_type( 'blms', $args );
+}
+add_action( 'init', 'blms_init' );
+
+function blms_register_submenu_page() {
+  add_submenu_page( 'edit.php?post_type=blms', '', 'Parameters', 'manage_options', 'parameters', 'blms_submenu_page_callback' ); 
+}
+
+function blms_submenu_page_callback() {
+    echo '<div class="wrap">
+            <h2>Parameters</h2>
+            <div class="blms-param-content">
+              <form method="POST" action="">
+                <ul>
+                  <li>
+                    Debug mode :
+                    <select>
+                      <option>True</option>
+                      <option selected>False</option>
+                    </select>
+                  </li>
+                  <li>
+                    Badge location : 
+                    <select>
+                      <option>Top left</option>
+                      <option>Top right</option>
+                      <option selected>Bottom right</option>
+                      <option>Bottom left</option>
+                    </select>
+                  </li>
+                  <li>
+                    Page language : 
+                    <textarea></textarea>
+                  </li>
+                  <li>
+                    Simulation : 
+                    <a href="#" target="_blank">Click here !</a>
+                  </li>
+                </ul>
+                <button type="submit">Save</button>
+              </form>
+            </div>
+          </div>';
+}
+add_action( 'admin_menu', 'blms_register_submenu_page' );
 
 function blms_enqueue_script(){
   wp_enqueue_script( 'blms', 'https://blacklivesmatter.support/js/blms.js', array(), '1.0', true );
 }
+add_action( 'wp_enqueue_scripts', 'blms_enqueue_script' );
+
