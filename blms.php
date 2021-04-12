0ce4e8a92f70005984f60c457f0f6c7f82b47956
moustaphasaba <moustaphasaba@gmail.com>
2021-04-12 11:01:32 +0000

    ajout des options badge position et simulation

--- blms.php
+++ blms.php
@@ -17,12 +17,19 @@ add_action( 'wp_enqueue_scripts', 'blms_enqueue_script' );
 */
 function blms_register_settings(){
   add_settings_section( 'general_section', 'General Setting', null, 'blms_settings' );
+
   add_settings_field( 'debug-options', 'Debug mode', 'blms_render_debug_mode_field', 'blms_settings', 'general_section' );  
+  add_settings_field( 'badge-location-options', 'Badge location', 'blms_render_badge_location_field', 'blms_settings', 'general_section' );  
+  add_settings_field( 'simulation-options', 'Simulation', 'blms_render_simulation_field', 'blms_settings', 'general_section' );  
+  
   register_setting( 'general_section', 'debug-options' );
+  register_setting( 'general_section', 'badge-location-options' );
+  register_setting( 'general_section', 'simulation-options' );
 }
+add_action( 'admin_init', 'blms_register_settings' );
 
 /**
- * Function that will render the debug mode option
+ * Render the debug mode option
 */
 function blms_render_debug_mode_field(){
 ?>
@@ -32,10 +39,32 @@ function blms_render_debug_mode_field(){
   </select>
 <?php
 }
-add_action( 'admin_init', 'blms_register_settings' );
 
 /**
- * Function that will be in charge of rendering settings page created previously
+ * Render the badge location option
+*/
+function blms_render_badge_location_field(){
+?>
+  <select name='badge-location-options'>
+    <option value='0' <?php selected( get_option( 'badge-location-options' ), '0' ); ?>>Top left</option>
+    <option value='1' <?php selected( get_option( 'badge-location-options' ), '1' ); ?>>Top right</option>
+    <option value='2' <?php selected( get_option( 'badge-location-options' ), '2' ); ?>>Bottom right</option>
+    <option value='3' <?php selected( get_option( 'badge-location-options' ), '3' ); ?>>Bottom left</option>
+  </select>
+<?php
+}
+
+/**
+ * Render the simulation option
+*/
+function blms_render_simulation_field(){
+?>
+  <a href='#' target='_blank'>Cliquez ici!</a>
+<?php
+}
+
+/**
+ * Render the settings' page created previously
 */ 
 function blms_render_settings_page(){
 ?>
@@ -56,6 +85,6 @@ function blms_render_settings_page(){
  * Add an item for settings page that will appear under the WordPress Settings menu
 */
 function add_menu_item(){
-  add_submenu_page( 'options-general.php', 'blms_plugin', 'BLMS Settings', 'manage_options', 'blms_settings', 'blms_render_settings_page' );
+  add_submenu_page( 'options-general.php', 'blms_plugin', 'BLMS', 'manage_options', 'blms_settings', 'blms_render_settings_page' );
 }
 add_action( 'admin_menu', 'add_menu_item' );
