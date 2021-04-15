eb8632ac0f4e4879d1e5d0212ead339b551b6850
moustaphasaba <moustaphasaba@gmail.com>
2021-04-15 11:29:23 +0000

    Ajout des options de langues et application des options choisies à travers le fichier javascript

--- blms.php
+++ blms.php
@@ -5,10 +5,19 @@
  * Version: 1.0
  * Author: Support BlackLivesMatter
  * Author URI: https://blacklivesmatter.support/
+ * Text Domain: blms
 */
 
 function blms_enqueue_script(){
-  wp_enqueue_script( 'blms', 'https://blacklivesmatter.support/js/blms.js', array(), '1.0', true );
+  //Get settings' options
+  $settings_options = array(
+    'debug' => get_option( 'debug-options' ),
+    'badge_location' => get_option( 'badge-location-options' ),
+    'language' => get_locale()
+  );
+
+  wp_enqueue_script( 'blms', plugins_url( 'blms.js', __file__ ), array(), '1.0', true );
+  wp_localize_script( 'blms', 'settings_options', $settings_options );
 }
 add_action( 'wp_enqueue_scripts', 'blms_enqueue_script' );
 
@@ -16,14 +25,16 @@ add_action( 'wp_enqueue_scripts', 'blms_enqueue_script' );
  * Register each of the options that will be part of our settings page
 */
 function blms_register_settings(){
-  add_settings_section( 'general_section', 'General Setting', null, 'blms_settings' );
+  add_settings_section( 'general_section', __( 'General Setting', 'blms' ), null, 'blms_settings' );
 
-  add_settings_field( 'debug-options', 'Debug mode', 'blms_render_debug_mode_field', 'blms_settings', 'general_section' );  
-  add_settings_field( 'badge-location-options', 'Badge location', 'blms_render_badge_location_field', 'blms_settings', 'general_section' );  
-  add_settings_field( 'simulation-options', 'Simulation', 'blms_render_simulation_field', 'blms_settings', 'general_section' );  
+  add_settings_field( 'debug-options', __( 'Debug mode', 'blms' ), 'blms_render_debug_mode_field', 'blms_settings', 'general_section' );  
+  add_settings_field( 'badge-location-options', __( 'Badge location', 'blms' ), 'blms_render_badge_location_field', 'blms_settings', 'general_section' );  
+  add_settings_field( 'language-options', __( 'Support language', 'blms' ), 'blms_render_language_field', 'blms_settings', 'general_section' );  
+  add_settings_field( 'simulation-options', __( 'Simulation', 'blms' ), 'blms_render_simulation_field', 'blms_settings', 'general_section' );  
   
   register_setting( 'general_section', 'debug-options' );
   register_setting( 'general_section', 'badge-location-options' );
+  register_setting( 'general_section', 'language-options' );
   register_setting( 'general_section', 'simulation-options' );
 }
 add_action( 'admin_init', 'blms_register_settings' );
@@ -32,10 +43,11 @@ add_action( 'admin_init', 'blms_register_settings' );
  * Render the debug mode option
 */
 function blms_render_debug_mode_field(){
+  $debug_mode_value = get_option( 'debug-options' );
 ?>
   <select name='debug-options'>
-    <option value='0' <?php selected( get_option( 'debug-options' ), '0' ); ?>>False</option>
-    <option value='1' <?php selected( get_option( 'debug-options' ), '1' ); ?>>True</option>
+    <option value=0 <?php selected( $debug_mode_value, 0 ); ?>><?php _e( 'False' )?></option>
+    <option value=1 <?php selected( $debug_mode_value, 1 ); ?>><?php _e( 'True' )?></option>
   </select>
 <?php
 }
@@ -44,14 +56,23 @@ function blms_render_debug_mode_field(){
  * Render the badge location option
 */
 function blms_render_badge_location_field(){
+  $badge_location_value = get_option( 'badge-location-options' );
 ?>
   <select name='badge-location-options'>
-    <option value='0' <?php selected( get_option( 'badge-location-options' ), '0' ); ?>>Top left</option>
-    <option value='1' <?php selected( get_option( 'badge-location-options' ), '1' ); ?>>Top right</option>
-    <option value='2' <?php selected( get_option( 'badge-location-options' ), '2' ); ?>>Bottom right</option>
-    <option value='3' <?php selected( get_option( 'badge-location-options' ), '3' ); ?>>Bottom left</option>
+    <option value='topleft' <?php selected( $badge_location_value, 'topleft' ); ?>><?php _e( 'Top left' )?></option>
+    <option value='topright' <?php selected( $badge_location_value, 'topright' ); ?>><?php _e( 'Top right' )?></option>
+    <option value='bottomright' <?php selected( $badge_location_value, 'bottomright' ); ?>><?php _e( 'Bottom right' )?></option>
+    <option value='bottomleft' <?php selected( $badge_location_value, 'bottomleft' ); ?>><?php _e( 'Bottom left' )?></option>
   </select>
 <?php
+}
+
+/**
+ * Render the badge location option
+*/
+function blms_render_language_field(){
+  $language_value = get_option( 'language-options' );
+  echo get_locale();
 }
 
 /**
@@ -59,7 +80,7 @@ function blms_render_badge_location_field(){
 */
 function blms_render_simulation_field(){
 ?>
-  <a href='#' target='_blank'>Cliquez ici!</a>
+  <a href='<?php echo get_home_url()."?blms_simulation";?>' target='_blank'><?php _e( 'Click here !' )?></a>
 <?php
 }
 
@@ -69,12 +90,12 @@ function blms_render_simulation_field(){
 function blms_render_settings_page(){
 ?>
   <div class='wrap'>
-      <h1>Black Lives Matter Plugin Settings</h1>
+      <h1><?php _e( 'Black Lives Matter Plugin Settings', 'blms' )?></h1>
       <form method='post' action='options.php'>
         <?php
           settings_fields( 'general_section' );
           do_settings_sections( 'blms_settings' );
-          submit_button();
+          submit_button( __( 'Save changes' ) );
         ?>
       </form>
   </div>
@@ -84,7 +105,23 @@ function blms_render_settings_page(){
 /**
  * Add an item for settings page that will appear under the WordPress Settings menu
 */
-function add_menu_item(){
+function blms_add_menu_item(){
   add_submenu_page( 'options-general.php', 'blms_plugin', 'BLMS', 'manage_options', 'blms_settings', 'blms_render_settings_page' );
 }
-add_action( 'admin_menu', 'add_menu_item' );
+add_action( 'admin_menu', 'blms_add_menu_item' );
+
+/**
+ * Display a notice to informe admin that the website will turns colorless in x days...
+*/
+function blms_admin_notice(){
+  //get the current screen
+  $screen = get_current_screen();
+  if( $screen->id === 'dashboard' ){ //Display the notice on the dashboard
+    if( date( 'm' ) == 5 && ( date( 'd' ) >= 15 && date( 'd' ) <= 25 ) ){ ?>
+      <div class="notice notice-info is-dismissible">
+          <p><?php _e( 'Your web site will turns colorless from 23 to 25 May.', 'blms' ) ?></p>
+      </div>
+    <?php }
+  }
+}
+add_action( 'admin_notices', 'blms_admin_notice' );
